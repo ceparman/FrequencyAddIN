d4cdfd1d38fe49e4dc610cf56dc195b5dc288b04
ceparman <info@ngsanalytics.com>
2020-11-10 11:34:33 -0500

    add comma??

--- R/RunFlowApp2.R
+++ R/RunFlowApp2.R
@@ -1,11 +1,11 @@
 #'RunFlowApp2 as a Rstudio addin
 #'
-#'\code{RunFlowApp2} run Run Imaging App as a Rstudio addin
+#'\code{RunFlowApp2} run Run Flow App as a Rstudio addin
 #'
 #'@export
 RunFlowApp2<- function() {
 
- shiny::runGitHub(repo="ceparman/Flow-App2",launch.browser=TRUE)
+ shiny::runGitHub(repo="ceparman/Flow-App2",,launch.browser=TRUE)
   return()
 }
 
